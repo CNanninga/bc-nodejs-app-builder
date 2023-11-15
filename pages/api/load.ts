d49b357cca309d77c2afd2ab49707f2c854e2132
Chris Nanninga <chris.nanninga@bigcommerce.com>
2023-11-15 17:10:04 -0600

    Update endpoints to use DB

--- pages/api/load.ts
+++ pages/api/load.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getBCVerify } from '../../lib/auth';
+import { encodePayload, getBCVerify, setSession } from '../../lib/auth';
 
 export default async function load(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await getBCVerify(req.query);
+        const session = await getBCVerify(req.query);
+        const encodedContext = encodePayload(session); // Signed JWT to validate/ prevent tampering
 
-        res.redirect(302, '/');
+        await setSession(session);
+        res.redirect(302, `/?context=${encodedContext}`);
     } catch (error) {
         const { message, response } = error;
         res.status(response?.status || 500).json(message);
