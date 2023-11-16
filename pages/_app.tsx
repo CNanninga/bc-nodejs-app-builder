df962f83e68439c412d6c9ff5aa34e4b822a0d46
Chris Nanninga <chris.nanninga@bigcommerce.com>
2023-11-15 18:14:07 -0600

    Update page frame

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import { GlobalStyles } from '@bigcommerce/big-design';
+import { Box, GlobalStyles } from '@bigcommerce/big-design';
+import Header from '../components/header';
 import type { AppProps } from 'next/app';
 import SessionProvider from '../context/session';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
     <>
         <GlobalStyles />
-        <SessionProvider>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <Box marginHorizontal="xxxLarge" marginVertical="xxLarge">
+            <Header />
+            <SessionProvider>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </Box>
     </>
  );
   
